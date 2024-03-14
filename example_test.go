39253d5be560ae775902a520d817e68256533fab
qdmc <108729292@qq.com>
2024-03-14 09:59:17 +0800

    test:ok

--- example_test.go
+++ example_test.go
@@ -1,6 +1,8 @@
 package go_stream_readWriter
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"testing"
 )
@@ -32,13 +34,99 @@ func ExampleBytesDivision() {
 	// 3
 
 }
-
 func ExampleNewReader() {
-
+	// 测试用报文
+	type testPacket struct {
+		U8     byte
+		U16    uint16
+		U32    uint32
+		U64    uint64
+		StrLen uint16
+		Str    string
+	}
+	p1 := testPacket{
+		U8:  8,
+		U16: 16,
+		U32: 32,
+		U64: 64,
+		Str: "this is a test packet",
+	}
+	p1.StrLen = uint16(len([]byte(p1.Str)))
+	var err error
+	w := NewWriter()
+	_, err = w.WriteUint8(p1.U8)
+	_, err = w.WriteUint16(p1.U16)
+	_, err = w.WriteUint32(p1.U32)
+	_, err = w.WriteUint64(p1.U64)
+	_, err = w.WriteUint16(p1.StrLen)
+	_, err = w.WriteBytes([]byte(p1.Str))
+	if err != nil {
+		fmt.Println("writer_err")
+	}
+	buf := bytes.NewReader(w.BufBytes())
+	p2 := new(testPacket)
+	r := NewReader()
+	p2.U8, err = r.ReadUint8(buf)
+	p2.U16, err = r.ReadUint16(buf)
+	p2.U32, err = r.ReadUint32(buf)
+	p2.U64, err = r.ReadUint64(buf)
+	strLen, err := r.ReadUint16(buf)
+	if err != nil {
+		fmt.Println("reader_err")
+	}
+	strBs, err := r.ReadBytes(buf, uint(strLen))
+	if err != nil {
+		fmt.Println("reader_err")
+	}
+	p2.Str = string(strBs)
+	fmt.Println(p1.U8 == p2.U8)
+	fmt.Println(p1.U16 == p2.U16)
+	fmt.Println(p1.U32 == p2.U32)
+	fmt.Println(p1.U64 == p2.U64)
+	fmt.Println(p1.Str == p2.Str)
+	// output: true
+	// true
+	// true
+	// true
+	// true
 }
 
 func ExampleNewWriter() {
-
+	var err error
+	order := binary.BigEndian
+	w := NewWriter()
+	w.SetOrder(order)
+	_, err = w.WriteUint8(8)
+	if err != nil {
+		fmt.Println("writer_err")
+	}
+	// 每一次调用BufBytes(),会有buf.Reset()
+	fmt.Println(w.BufBytes()[0])
+	_, err = w.WriteUint16(16)
+	if err != nil {
+		fmt.Println("writer_err")
+	}
+	fmt.Println(order.Uint16(w.BufBytes()))
+	_, err = w.WriteUint32(32)
+	if err != nil {
+		fmt.Println("writer_err")
+	}
+	fmt.Println(order.Uint32(w.BufBytes()))
+	_, err = w.WriteUint64(64)
+	if err != nil {
+		fmt.Println("writer_err")
+	}
+	fmt.Println(order.Uint64(w.BufBytes()))
+	_, err = w.WriteBytes([]byte("hello"))
+	if err != nil {
+		fmt.Println("writer_err")
+	}
+	fmt.Println(string(w.BufBytes()))
+	// output: 8
+	// 16
+	// 32
+	// 64
+	// hello
 }
 
 func Test_bits(t *testing.T) {
